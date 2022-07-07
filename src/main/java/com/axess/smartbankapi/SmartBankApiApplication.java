2b1d0c35dcf410abfacb7abdcb89b11ba17d9840
chaijunkin <chaijunkin@gmail.com>
2022-07-07 12:20:38 +0800

    add enable scheduling to application

--- src/main/java/com/axess/smartbankapi/SmartBankApiApplication.java
+++ src/main/java/com/axess/smartbankapi/SmartBankApiApplication.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.axess.smartbankapi.model.CCUser;
 import com.axess.smartbankapi.model.RewardsCatalogue;
